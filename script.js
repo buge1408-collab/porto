b49fbf922e57c50866e6118d154ea032b7205482
buge1408-collab <buge1408@gmail.com>
2026-08-18 21:15:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("M Buge Munawar Portfolio Loaded");
+// Fade In Animation
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.15
+});
+
+document.querySelectorAll(".hidden").forEach((el)=>{
+    observer.observe(el);
+});
+
+// Navbar Shadow
+
+window.addEventListener("scroll",()=>{
+
+    const nav = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        nav.classList.add("active-nav");
+    }else{
+        nav.classList.remove("active-nav");
+    }
+
+});
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href'))
+        .scrollIntoView({
+            behavior:'smooth'
+        });
+
+    });
+
+});
